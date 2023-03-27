2e3e00fc5b179b830b1aedb6a35d1e9b64f5da8f
Bjorn Worrall <85297118+lasity34@users.noreply.github.com>
2023-03-27 15:34:37 +0200

    years ago fixed

--- .history/test/yearsAgo.tests_20230327153118.js
+++ .history/test/yearsAgo.tests_20230327153118.js
@@ -1,9 +1,9 @@
 describe("Test my yearsAgo function", function () {
   it("It should return the difference between currentYear minus previousYear", function () {
-    assert.equal(34, yearsAgo(1989), "This should be true");
+    assert.equal(23, yearsAgo(1989), "This should be true");
   });
 
   it("It should return the difference between currentYear minus previousYear", function () {
-    assert.equal(19, yearsAgo(2004), "This should be true");
+    assert.equal((new Date().getFullYear() - 2004), yearsAgo(2004), "This should be true");
   });
 });
